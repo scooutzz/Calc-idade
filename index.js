74e6f0b0d90c9e912afe87fa64d846a1bb51e1dc
Fabio lazaro <fabioalmeida2004@gmail.com>
2021-08-24 23:12:54 -0300

    edit calc idade + frutas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var nome = "Fabio"
 
 
 var hoje = new Date();
-var aniversario = new Date('1979-08-22');
+var aniversario = new Date('2004-08-07');
 
 
 var ano = hoje.getFullYear() - aniversario.getFullYear();
@@ -11,10 +11,31 @@ var mes = hoje.getMonth() - aniversario.getMonth();
 if (mes < 0) {
     ano = ano - 1;
 }
-var dia = hoje.getDay() - aniversario.getDay();
-if (dia > 0) {
+var dia = hoje.getDate() - aniversario.getDate();
+if (dia < 0 && mes == 0) {
     ano = ano - 1;
 }
 
 console.log('Nome: ', nome);
-console.log("\nIdade: ", ano, "anos");
+console.log("\nIdade: ", ano, "ano(s)");
+
+let frutas = ['uva', 'banana', 'maça', 'laranja', 'pêra']
+for (let indice = 0; indice < frutas.length; indice++) {
+    const frutasCorrente = frutas[indice];
+
+    if (frutasCorrente == 'uva') {
+        console.log("A " + frutasCorrente + " é roxa.");
+    }
+    if (frutasCorrente == 'banana') {
+        console.log("A " + frutasCorrente + " é amarela.");
+    }
+    if (frutasCorrente == 'maça') {
+        console.log("A " + frutasCorrente + " é vermelha.");
+    }
+    if (frutasCorrente == 'laranja') {
+        console.log("A " + frutasCorrente + " é laranja.");
+    }
+    if (frutasCorrente == 'pêra') {
+        console.log("A " + frutasCorrente + " é amarela.");
+    }
+}
